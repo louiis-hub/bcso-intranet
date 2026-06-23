903c8b673435bfa2b27e968ab8cd78b1762c07c2
louiis-hub <louis.herrou1@gmail.com>
2026-06-24 01:49:20 +0200

    Force Discord consent prompt to prevent wrong account reuse

--- db.js
+++ db.js
@@ -13,7 +13,8 @@ var DB = {
       provider: 'discord',
       options: {
         scopes: 'identify guilds.members.read',
-        redirectTo: 'https://louiis-hub.github.io/bcso-intranet/'
+        redirectTo: 'https://louiis-hub.github.io/bcso-intranet/',
+        queryParams: { prompt: 'consent' }
       }
     });
   },
